c6379b16377e34ec60734840215a603360fdb90f
Richard Moe <richmoe07@gmail.com>
2017-07-20 19:28:18 -0500

    small changes
    start button
    need to fix wait time

---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class StartButton : MonoBehaviour {
+
+    public GameObject thisbutton;
+	// Use this for initialization
+
+	void Start () {
+        StartCoroutine(reactive());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("BossOne");
+    }
+
+
+    IEnumerator reactive()
+    {
+        //Fix This
+        thisbutton.SetActive(false);
+        yield return new WaitForSeconds(3);
+        print(Time.time);
+        thisbutton.SetActive(true);
+
+    }
+
+}
